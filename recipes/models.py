8a4fc68b143c0528e8e1d079259a27bb3c34725b
wilson <wilsnyeh@gmail.com>
2022-04-20 12:52:27 -0700

    created class for receipe

--- recipes/models.py
+++ recipes/models.py
@@ -2,3 +2,13 @@ from django.db import models
 
 
 # Create your models here.
+class Receipe(models.Model):
+    name = models.CharField(max_length=125)
+    author = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.URLField(null=True, blank=True)
+    created = models.DateTimeField(auto_now=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
