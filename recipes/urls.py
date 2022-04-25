3aaa4584a81a16e090bf47ebba34152deedc0a6b
wilson <wilsnyeh@gmail.com>
2022-04-25 15:51:24 -0700

    updated html files

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from recipes.views import (
     RecipeCreateView,
+    RecipeDeleteView,
     RecipeUpdateView,
     log_rating,
     RecipeDetailView,
@@ -11,7 +12,8 @@ from recipes.views import (
 urlpatterns = [
     path("", RecipeListView.as_view(), name="recipes_list"),
     path("<int:pk>/", RecipeDetailView.as_view(), name="recipe_detail"),
+    path("<int:pk>/delete/", RecipeDeleteView.as_view(), name="recipe_delete"),
     path("new/", RecipeCreateView.as_view(), name="recipe_new"),
-    path("edit/", RecipeUpdateView.as_view(), name="recipe_edit"),
+    path("<int:pk>/edit/", RecipeUpdateView.as_view(), name="recipe_edit"),
     path("<int:recipe_id>/ratings/", log_rating, name="recipe_rating"),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 from recipes.forms import RatingForm
 
@@ -99,4 +99,10 @@ class RecipeUpdateView(UpdateView):
     model = Recipe
     template_name = "recipes/edit.html"
     fields = ["name", "author", "description", "image"]
+    success_url = reverse_lazy("recipes_list")
+
+
+class RecipeDeleteView(DeleteView):
+    model = Recipe
+    template_name = "recipes/delete.html"
     success_url = reverse_lazy("recipes_list")
