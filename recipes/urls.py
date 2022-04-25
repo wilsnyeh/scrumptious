5f388ee52188da63344070344174f9d9aedf7a75
wilson <wilsnyeh@gmail.com>
2022-04-25 14:50:58 -0700

    added the update view for reipe

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from recipes.views import (
     RecipeCreateView,
-    change_recipe,
+    RecipeUpdateView,
     log_rating,
     RecipeDetailView,
     RecipeListView,
@@ -12,6 +12,6 @@ urlpatterns = [
     path("", RecipeListView.as_view(), name="recipes_list"),
     path("<int:pk>/", RecipeDetailView.as_view(), name="recipe_detail"),
     path("new/", RecipeCreateView.as_view(), name="recipe_new"),
-    path("edit/", change_recipe, name="recipe_edit"),
+    path("edit/", RecipeUpdateView.as_view(), name="recipe_edit"),
     path("<int:recipe_id>/ratings/", log_rating, name="recipe_rating"),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 
 from recipes.forms import RatingForm
 
@@ -30,22 +30,22 @@ except Exception:
 #     return render(request, "recipes/new.html", context)
 
 
-def change_recipe(request, pk):
-    if Recipe and RecipeForm:
-        instance = Recipe.objects.get(pk=pk)
-        if request.method == "POST":
-            form = RecipeForm(request.POST, instance=instance)
-            if form.is_valid():
-                form.save()
-                return redirect("recipe_detail", pk=pk)
-        else:
-            form = RecipeForm(instance=instance)
-    else:
-        form = None
-    context = {
-        "form": form,
-    }
-    return render(request, "recipes/edit.html", context)
+# def change_recipe(request, pk):
+#     if Recipe and RecipeForm:
+#         instance = Recipe.objects.get(pk=pk)
+#         if request.method == "POST":
+#             form = RecipeForm(request.POST, instance=instance)
+#             if form.is_valid():
+#                 form.save()
+#                 return redirect("recipe_detail", pk=pk)
+#         else:
+#             form = RecipeForm(instance=instance)
+#     else:
+#         form = None
+#     context = {
+#         "form": form,
+#     }
+#     return render(request, "recipes/edit.html", context)
 
 
 # def show_recipes(request):
@@ -91,5 +91,12 @@ class RecipeDetailView(DetailView):
 class RecipeCreateView(CreateView):
     model = Recipe
     template_name = "recipes/new.html"
+    fields = ["name", "author", "description", "image"]
+    success_url = reverse_lazy("recipes_list")
+
+
+class RecipeUpdateView(UpdateView):
+    model = Recipe
+    template_name = "recipes/edit.html"
     fields = ["name", "author", "description", "image"]
     success_url = reverse_lazy("recipes_list")
