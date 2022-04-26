f01d648a7868751a8e0e36627a123391a20769d3
wilson <wilsnyeh@gmail.com>
2022-04-26 11:17:02 -0700

    removed unnecessary try-except blocks

--- recipes/forms.py
+++ recipes/forms.py
@@ -1,19 +1,19 @@
 from django import forms
 
-try:
-    from recipes.models import Recipe
 
-    class RecipeForm(forms.ModelForm):
-        class Meta:
-            model = Recipe
-            fields = [
-                "name",
-                "author",
-                "description",
-                "image",
-            ]
+from recipes.models import Recipe
 
-except Exception:
+
+class RecipeForm(forms.ModelForm):
+    class Meta:
+        model = Recipe
+        fields = [
+            "name",
+            "author",
+            "description",
+            "image",
+        ]
+
     pass
 
 
--- recipes/views.py
+++ recipes/views.py
@@ -1,17 +1,64 @@
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
-from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
+from django.views.generic import ListView
 
 from recipes.forms import RatingForm
 
-try:
-    from recipes.forms import RecipeForm
-    from recipes.models import Recipe
-except Exception:
-    RecipeForm = None
-    Recipe = None
+from recipes.forms import RecipeForm
+from recipes.models import Recipe
+
+
+# def create_recipe(request):
+#     if request.method == "POST" and RecipeForm:
+#         form = RecipeForm(request.POST)
+#         if form.is_valid():
+#             recipe = form.save()
+#             return redirect("recipe_detail", pk=recipe.pk)
+#     elif RecipeForm:
+#         form = RecipeForm()
+#     else:
+#         form = None
+#     context = {
+#         "form": form,
+#     }
+#     return render(request, "recipes/new.html", context)
+
+
+# def change_recipe(request, pk):
+#     if Recipe and RecipeForm:
+#         instance = Recipe.objects.get(pk=pk)
+#         if request.method == "POST":
+#             form = RecipeForm(request.POST, instance=instance)
+#             if form.is_valid():
+#                 form.save()
+#                 return redirect("recipe_detail", pk=pk)
+#         else:
+#             form = RecipeForm(instance=instance)
+#     else:
+#         form = None
+#     context = {
+#         "form": form,
+#     }
+#     return render(request, "recipes/edit.html", context)
+
+
+# def show_recipes(request):
+#     context = {
+#         "recipes": Recipe.objects.all() if Recipe else [],
+#     }
+#     return render(request, "recipes/list.html", context)
+
+
+# def show_recipe(request, pk):
+#     context = {
+#         "recipe": Recipe.objects.get(pk=pk) if Recipe else None,
+#         "rating_form": RatingForm(),  # highlight
+#     }
+#     return render(request, "recipes/detail.html", context)
 
 
 def log_rating(request, recipe_id):
@@ -36,7 +83,7 @@ class RecipeDetailView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["rating_form"] = RatingForm()
+        context["raing_form"] = RatingForm()
         return context
 
 
